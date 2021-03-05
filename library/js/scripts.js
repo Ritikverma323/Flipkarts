d512a81bfbaf61ab1c909cc250143bcbce78718a
user <iritikverma7@gmail.com>
2021-03-05 12:52:16 +0530

    initial commits

---
+++ library/js/scripts.js
@@ -0,0 +1,33 @@
+
+    $(".banner__wrapper").slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      dots: false,
+
+      prevArrow: '<i class=" slick-prev fa fa-caret-left "></i>',
+      nextArrow: '<i class=" slick-next fa fa-caret-right"></i>',
+    });
+
+    $(".deals__product").slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+
+      prevArrow: '<i class=" slick-prev-deals fa fa-caret-left "></i>',
+      nextArrow: '<i class=" slick-next-deals fa fa-caret-right"></i>',
+    });
+
+    $(".products_areas ").slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+
+      prevArrow: '<i class=" slick-prev-deals fa fa-caret-left "></i>',
+      nextArrow: '<i class=" slick-next-deals fa fa-caret-right"></i>',
+    });
+ 
+   
